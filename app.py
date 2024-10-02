f3811f065e7b3d580d30a1709504702e86420e93
Dobi Nándor <nandordobi45@gmail.com>
2024-10-02 13:51:15 +0200

    Learned about "for" and "while" loops

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 a: bool = True
 b: bool = False
 
-
+# Learn "if" statement
 """ 
 if a:
     print("a is True")
@@ -9,4 +9,27 @@ elif b:
     print("b os True")
 else:
     print("Neither a or b is true")
+ """
+
+# What is "for" loop
+# C proggraming language: for(int i = 0; i < 5; i++)
+
+""" 
+for i in range(5):
+    print(i)
+ """
+
+# What is "while" loop
+
+""" message: str = "Give me your answer: "
+
+while True:
+    answer: str = input(message)
+    if len(answer) >= 3:
+        print("Thanks!")
+        # Break out of the while loop
+        break
+
+    else:
+        message = "Please give me an answer thats atleast 3 characters long!"
  """
